77f3c056507797bb66fb60443c10892c7675e117
thequizzmaster <151899578+thequizzmaster@users.noreply.github.com>
2023-11-24 16:22:41 +1100

    Add files via upload

---
+++ Challenge 1.py
@@ -0,0 +1,78 @@
+#                Challenge 1 - The Christmas Tree
+
+
+# ************************* Part One *****************************
+
+
+# Oh no! Santa needs a Christmas tree for their Christmas party.
+# They need it quick! They need it pronto!
+#
+# The Christmas tree santa needs looks like this:
+#
+#                              *
+#                             /^\
+#                            /^^^\
+#                           /^^^^^\
+#                             |||
+#
+# It has a star on top, the edges of the Christmas tree are formed
+# from slashes, the interior of the tree is filled with carets (^)
+# and the trunk of the tree is formed by three straight lines
+# (press shift + the key below backspace)
+#
+# Quickly! Write a program that prints this Christmas tree to the
+# console so santa can be ready for Christmas!
+
+
+# ************************* Part Two *****************************
+
+
+# Oh no! Those pesky elves must have stolen some trees! Santa
+# needs a lot more than one Christmas tree! Santa needs
+# Christmas trees of size 6, 7, 8, 9, and 10!
+#
+# The size of a Christmas tree is the total height of the tree in
+# lines. The tree printed above has a size of 5, the below tree
+# has a size of 6:
+#
+#                              *
+#                             /^\
+#                            /^^^\
+#                           /^^^^^\
+#                          /^^^^^^^\
+#                             |||
+#
+# Note that as the tree gets bigger it gets wider to retain its
+# unique shape, but the trunk and the star stay the same and
+# stay centered.
+#
+# Quickly! Write a program that prints a Christmas tree of size
+# 5, 6, 7, 8, 9, and 10 to the console so that Santa isn't
+# embarrassed at their party!
+#
+# Santa will give a present to the person who writes the program
+# which executes the fastest! Submissions will end Friday 24/11.
+#
+#
+# Due to North Pole compliance reasons all programs will be run
+# in python. If it's written in a different language, elves will
+# port it into python (potentially poorly).
+
+
+# ****************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
